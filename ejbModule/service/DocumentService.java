72e5b329c7892fd6741041ef52562c3558e4ab73
mak <bmakhlouf@hotmail.fr>
2016-03-13 12:55:23 +0100

    Refactoring couche serveur.

--- ejbModule/service/DocumentService.java
+++ ejbModule/service/DocumentService.java
@@ -6,41 +6,39 @@ import javax.ejb.EJB;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
+import dao.IDocumentDao;
 import entity.Document;
 
 @Stateless
 @Remote(IDocumentService.class)
 public class DocumentService implements IDocumentService{
-
-	@EJB
-	GererDocumentService gererService;
 	
 	@EJB
-	ListerDocumentService listerService;
+	IDocumentDao documentDao;
 	
 	@Override
 	public void createDocument(Document document) {
-		gererService.createDocument(document);;
+		documentDao.createDocument(document);;
 	}
 
 	@Override
 	public void deleteDocument(int cote) {
-		gererService.deleteDocument(cote);
+		documentDao.deleteDocument(cote);
 	}
 
 	@Override
 	public Document findById(int cote) {
-		return gererService.findById(cote);
+		return documentDao.findById(cote);
 	}
 
 	@Override
 	public void updateDocument(Document document) {
-		gererService.updateDocument(document);
+		documentDao.updateDocument(document);
 	}
 
 	@Override
 	public List<Document> getAllDocument() {
-		return listerService.getAllDocument();
+		return documentDao.getAllDocument();
 	}
 
 }
